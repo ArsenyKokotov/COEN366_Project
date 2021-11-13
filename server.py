be77fc764c107d59c89e114f4ffb8d98c0bcf05c
ArsenyKokotov <52434320+ArsenyKokotov@users.noreply.github.com>
2021-11-13 11:30:29 -0500

    Update server.py

--- server.py
+++ server.py
@@ -183,7 +183,7 @@ def handle_client(message, address):
         retrieve_infot(message_json, addr)
     elif service_type == "SEARCH-FILE":
         search_file(message_json, addr)
-    elif service_type == "UPDATE CONTACT":
+    elif service_type == "UPDATE-CONTACT":
         update_contact(message_json, addr)
 
     # msg="Message Received"
